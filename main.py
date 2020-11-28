03c1b1a8f66fdcd90f98dc8b7ef62f9a33e76123
David Gong <davidgong99@gmail.com>
2020-11-28 17:20:29 +1100

    add templates for all required functions

--- main.py
+++ main.py
@@ -1,15 +1,10 @@
 from flask import Flask, jsonify
-# from json import JSONEncoder, json
 import json
 from json import JSONEncoder
 
 class SpaceshipEncoder(JSONEncoder):
     def default(self, o):
             return o.__dict__
-
-# class Encoder(JSONEncoder):
-#     def default(self, o):
-#         return o.__dict__
 
 
 # key = id, value = Spaceship (object)
@@ -32,10 +27,20 @@ class Spaceship:
 
 app = Flask(__name__)
 
+# ========================
+# =
+# =     Default route
+# =
+# ========================
 @app.route('/')
 def hello():
     return 'Welcome to Icarus'
 
+# ========================
+# =
+# =     Add spaceship
+# =
+# ========================
 @app.route('/addShip')
 def addSpaceship():
     s = Spaceship('Name','MyModel','Thislocation','Maintenance')
@@ -45,11 +50,71 @@ def addSpaceship():
 
     return s.toJSON()
 
+# ========================
+# =
+# =      List spaceships (NOT REQUIRED)
+# =
+# ========================
 @app.route('/listShips')
 def listShips():
     jsonStr = json.dumps(ships, indent=4, cls=SpaceshipEncoder)
     print(jsonStr)
     return jsonStr
+
+# ========================
+# =
+# =      TODO: Update spaceship
+# =
+# ========================
+# TODO: make PUT request
+# TODO: add queries
+@app.route('/updateShip')
+def updateShip():
+    return "/updateShip"
+
+# ========================
+# =
+# =      TODO: Add location
+# =
+# ========================
+# TODO: make POST request
+# TODO: add queries
+@app.route('/addLocation')
+def addLocation():
+    return "/addLocation"
+
+# ========================
+# =
+# =      TODO: Remove spaceship
+# =
+# ========================
+# TODO: make DEL request
+# TODO: add queries
+@app.route('/removeShip')
+def removeShip():
+    return "/removeShip"
+
+# ========================
+# =
+# =      TODO: Remove location
+# =
+# ========================
+# TODO: make DEL request
+# TODO: add queries
+@app.route('/removeLocation')
+def removeLocation():
+    return "/removeLocation"
+
+# ========================
+# =
+# =      TODO: Travel
+# =
+# ========================
+# TODO: make POST request
+# TODO: add queries
+@app.route('/travel')
+def travel():
+    return "/travel"
 
 if __name__ == '__main__':
     app.debug = True
