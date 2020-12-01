726158eeae4ac9d9468040add5de284856818930
David Gong <davidgong99@gmail.com>
2020-12-01 14:35:35 +1100

    render ships table

--- main.py
+++ main.py
@@ -146,7 +146,7 @@ app = Flask(__name__)
 # ========================
 @app.route('/')
 def hello():
-    return render_template('index.html')
+    return render_template('ships.html')
     return 'Welcome to Icarus', 200
 
 # ========================
